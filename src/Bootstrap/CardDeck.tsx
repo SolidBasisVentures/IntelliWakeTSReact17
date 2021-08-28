321abbf987905c846aaa7f61f7b62d524fc77bdc
Dennis Peters <denjpeters@gmail.com>
2021-08-28 18:43:53 -0400

    Updated carddeck

--- src/Bootstrap/CardDeck.tsx
+++ src/Bootstrap/CardDeck.tsx
@@ -1,14 +1,18 @@
 import React from 'react'
 import {OmitProperty} from '@solidbasisventures/intelliwaketsfoundation'
+import {TFieldSetBreakAt} from './FieldSet'
 
 export interface IWCardDeckProps extends Omit<React.HTMLProps<HTMLDivElement>, 'ref'> {
 	tag?: string | React.ReactType
+	breakAt?: TFieldSetBreakAt
+	smallRows?: number
+	largeRows?: number
 }
 
 export const CardDeck = (props: IWCardDeckProps) => {
 	const TagToUse = props.tag ?? ('div' as React.ReactType)
 
 	return (
-		<TagToUse {...OmitProperty(props, 'tag', 'className')} className={`card-deck ${props.className ?? ''}`.trim()} />
+		<TagToUse {...OmitProperty(props, 'tag', 'className')} className={`card-deck ${!props.breakAt ? `row-cols-${props.smallRows ?? 'auto'}` : `row-cols-${props.breakAt}-${props.largeRows ?? 'auto'} row-cols-${props.smallRows ?? 'auto'}`} row ${props.className ?? ''}`.trim()} />
 	)
 }
