554ccda99ddbad15b7b327038ccd74c70d8bf00c
Dennis Peters <denjpeters@gmail.com>
2022-04-12 11:07:57 -0400

    Remove consoles

--- src/WebControls/InputDate.tsx
+++ src/WebControls/InputDate.tsx
@@ -57,11 +57,9 @@ export function InputDate<T>(props: IProps<T>) {
 	const handleBlur = (e: React.FocusEvent<HTMLInputElement>) => {
 		// nextDateValue.current = MomentDateString(e.target.value) ?? ''
 		
-		console.log('Should blur', !!nextDateValue.current, !!props.changeValue)
 		if (nextDateValue.current && props.changeValue) {
 			const dateObj = DateObject(nextDateValue.current)
 			const enteredYear = dateObj?.getUTCFullYear() ?? 0
-		console.log('Blur year', dateObj, enteredYear)
 			if (dateObj && enteredYear < 100) {
 				const currentYear = new Date().getUTCFullYear()
 				const currentCentury = Math.floor(currentYear / 100) * 100
