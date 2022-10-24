60710eb072c091e814ac7194f6bd1e318fd73bc3
Dennis Peters <denjpeters@gmail.com>
2022-10-24 11:43:10 -0400

    DateRangeRefactoring

--- src/WebControls/DateRange.tsx
+++ src/WebControls/DateRange.tsx
@@ -43,13 +43,13 @@ export interface IDateRange {
 	end: Moment
 }
 
-export const initialDateRange: IDateRange = {
+export const InitialDateRange = (): IDateRange => ({
 	name: customRangeName,
 	start: moment(),
 	end: moment()
-}
+})
 
-export const initialDateRangeString: IDateRangeString = DateRangeToString(initialDateRange)
+export const InitialDateRangeString: IDateRangeString = DateRangeToString(InitialDateRange())
 
 interface IPropsCalendar {
 	month: Moment,
@@ -62,81 +62,81 @@ interface IPropsCalendar {
 
 export const DateRangeCalendar = (props: IPropsCalendar) => {
 	let moments: (Moment[])[] = []
-	
+
 	let firstDay = props.month.clone().startOf('month')
 	let currentDay = firstDay.clone().startOf('week')
 	let lastDay = props.month.clone().endOf('month')
-	
+
 	while (currentDay.isBefore(lastDay)) {
 		let week: Moment[] = []
-		
+
 		do {
 			week.push(currentDay.clone())
 			currentDay.add(1, 'day')
 		} while (currentDay.weekday() > 0)
-		
+
 		moments.push(week)
 	}
-	
+
 	const prev = () => {
 		if (props.prevMonth) {
 			props.prevMonth()
 		}
 	}
-	
+
 	const next = () => {
 		if (props.nextMonth) {
 			props.nextMonth()
 		}
 	}
-	
+
 	return (
-		<table>
-			<thead>
-			<tr>
-				{props.prevMonth !== undefined
-					?
-					<th className="prev available" onClick={prev}><span> </span></th>
-					:
-					<th />
-				}
-				<th colSpan={5} className="month">{firstDay.format('MMM YYYY')}</th>
-				{props.nextMonth !== undefined
-					?
-					<th className="next available" onClick={next}><span> </span></th>
-					:
-					<th />
-				}
-			</tr>
-			<tr>
-				<th>Su</th>
-				<th>Mo</th>
-				<th>Tu</th>
-				<th>We</th>
-				<th>Th</th>
-				<th>Fr</th>
-				<th>Sa</th>
-			</tr>
-			</thead>
-			<tbody>
-			{moments.map((week, idx: number) =>
-				<tr key={idx}>
-					{week.map((day) =>
-						<td className={
-							(day.format('dd') === 'Sa' || day.format('dd') === 'Su' ? 'weekend ' : '') +
-							((day.isBefore(firstDay, 'day') || day.isAfter(lastDay, 'day')) && !day.isBetween(props.startSelected, props.endSelected, 'day', '[]') ? 'off ends ' : '') +
-							(day.isSame(props.startSelected, 'day') ? 'active start-date ' : '') +
-							(day.isBetween(props.startSelected, props.endSelected, 'day') ? 'in-range ' : '') +
-							(day.isSame(props.endSelected, 'day') ? 'active end-date ' : '') +
-							'available '
-						} key={day.format()} onClick={() => props.dateClick(day)}>
-							{day.format('D')}
-						</td>
-					)}
+			<table>
+				<thead>
+				<tr>
+					{props.prevMonth !== undefined
+							?
+							<th className='prev available' onClick={prev}><span> </span></th>
+							:
+							<th />
+					}
+					<th colSpan={5} className='month'>{firstDay.format('MMM YYYY')}</th>
+					{props.nextMonth !== undefined
+							?
+							<th className='next available' onClick={next}><span> </span></th>
+							:
+							<th />
+					}
 				</tr>
-			)}
-			</tbody>
-		</table>
+				<tr>
+					<th>Su</th>
+					<th>Mo</th>
+					<th>Tu</th>
+					<th>We</th>
+					<th>Th</th>
+					<th>Fr</th>
+					<th>Sa</th>
+				</tr>
+				</thead>
+				<tbody>
+				{moments.map((week, idx: number) =>
+						<tr key={idx}>
+							{week.map((day) =>
+									<td className={
+											(day.format('dd') === 'Sa' || day.format('dd') === 'Su' ? 'weekend ' : '') +
+											((day.isBefore(firstDay, 'day') || day.isAfter(lastDay, 'day')) && !day.isBetween(props.startSelected, props.endSelected, 'day', '[]') ? 'off ends ' : '') +
+											(day.isSame(props.startSelected, 'day') ? 'active start-date ' : '') +
+											(day.isBetween(props.startSelected, props.endSelected, 'day') ? 'in-range ' : '') +
+											(day.isSame(props.endSelected, 'day') ? 'active end-date ' : '') +
+											'available '
+									} key={day.format()} onClick={() => props.dateClick(day)}>
+										{day.format('D')}
+									</td>
+							)}
+						</tr>
+				)}
+				</tbody>
+			</table>
 	)
 }
 
@@ -156,22 +156,22 @@ export interface IPropsDateRange {
 export const DateRange = (props: IPropsDateRange) => {
 	const nodeParent: any = useRef()
 	const nodeBody: any = useRef()
-	
+
 	const getStartRange = (): IDateRange => {
 		if (props.defaultRange && props.defaultRange.name) {
 			if (props.defaultRange.name === customRangeName) {
 				return DateRangeToMoment(props.defaultRange)
 			}
-			
+
 			if (!!props.presetRanges) {
 				const presetRanges = props.presetRanges.map(range => DateRangeToMoment(range))
-				
+
 				if (presetRanges.length > 0) {
 					const foundItem = presetRanges.find((item: IDateRange) => props.defaultRange!.name === item.name)
 					if (foundItem) {
 						return foundItem
 					}
-					
+
 					const foundItemStartsWith = presetRanges.find((item) => item.name.startsWith(props.defaultRange!.name))
 					if (foundItemStartsWith) {
 						return foundItemStartsWith
@@ -179,154 +179,160 @@ export const DateRange = (props: IPropsDateRange) => {
 				}
 			}
 		}
-		
+
 		if (props.presetRanges && props.presetRanges.length > 0) return DateRangeToMoment(props.presetRanges[0])
-		
-		return initialDateRange
+
+		return InitialDateRange()
 	}
-	
+
 	const [state, setState] = useState({
 		isOpen: false,
 		selectedRange: getStartRange(),
 		selectedText: '',
 		prevPreset: null as IDateRange | null,
-		customRange: initialDateRange,
+		customRange: InitialDateRange(),
 		monthToShow: getStartRange().start,
 		applyToFirst: true
 	})
-	
+
 	const getCurrentRange = (): IDateRange => {
 		if (state.selectedRange) return state.selectedRange
-		
+
 		return getStartRange()
 	}
-	
+
 	const currentRange = getCurrentRange()
-	
+
 	const rangeDescription = (range: IDateRange): string => {
 		return (range.name === customRangeName ? (moment(range.start).format('L') + ' - ' + moment(range.end).format('L')) : range.name)
 	}
-	
+
 	const setOpen = (e: any) => {
 		if (!nodeBody.current.contains(e.target)) {
 			setState({...state, isOpen: true})
 		}
 	}
-	
+
 	const handleClick = (e: any) => {
 		if (!nodeParent.current.contains(e.target)) {
 			setState({...state, isOpen: false})
 		}
 	}
-	
+
 	const handlePresetClick = (range: IDateRange) => {
 		setState({...state, isOpen: false, selectedRange: range})
-		
+
 		if (!!props.selectRange) props.selectRange(range)
 		if (!!props.selectRangeString) props.selectRangeString(DateRangeToString(range))
 	}
-	
+
 	const handleCustomApplyClick = () => {
 		setState({...state, isOpen: false, selectedRange: state.customRange})
-		
+
 		if (!!props.selectRange) props.selectRange(state.customRange)
 		if (!!props.selectRangeString) props.selectRangeString(DateRangeToString(state.customRange))
 	}
-	
+
 	const handleCustomClick = () => {
 		const customRange = {...getCurrentRange(), name: customRangeName}
-		
+
 		setState({...state, prevPreset: currentRange, customRange: customRange})
 	}
-	
+
 	const handleUnCustomClick = () => {
 		const customRange = {...getCurrentRange(), name: customRangeName}
-		
+
 		setState({...state, prevPreset: null, customRange: customRange})
 	}
-	
+
 	const handleDateClick = (day: Moment) => {
 		let newState = {...state}
-		
+
 		if (newState.applyToFirst) {
 			newState.customRange.start = day
 		} else {
 			newState.customRange.end = day
 		}
-		
+
 		if (newState.customRange.start.isAfter(newState.customRange.end)) {
 			[newState.customRange.start, newState.customRange.end] = [newState.customRange.end, newState.customRange.start]
 		}
-		
+
 		newState.applyToFirst = !newState.applyToFirst
-		
+
 		setState(newState)
 	}
-	
+
 	const prevMonth = () => {
 		const prev = state.monthToShow.clone().subtract(1, 'month')
 		setState({...state, monthToShow: prev})
 	}
-	
+
 	const nextMonth = () => {
 		const next = state.monthToShow.clone().add(1, 'month')
 		setState({...state, monthToShow: next})
 	}
-	
+
 	useEffect(() => {
 		document.addEventListener('mousedown', handleClick)
 		return () => {
 			document.removeEventListener('mousedown', handleClick)
 		}
 	})
-	
+
 	useEffect(() => {
 		if (!!props.defaultRange) {
 			setState({...state, selectedRange: DateRangeToMoment(props.defaultRange)})
 		}
 	}, [props.defaultRange])
-	
+
 	return (
-		<div className={'DateRangeDD ' + (props.className ?? '') + (props.borderless ? '' : ' border') + (props.showCaret ? ' dropdown-toggle' : '')}
-		     onClick={setOpen} ref={nodeParent} color={props.color}>
-			{props.faIcon !== null &&
-			<FontAwesomeIcon icon={props.faIcon ? props.faIcon : faCalendarAlt} fixedWidth />
-			} {rangeDescription(state.selectedRange!)}
-			<div className={ClassNames({DateRangeLB: true, OpensRight: !props.rightAlign, 'd-none': !state.isOpen})} ref={nodeBody}>
-				<div className={'ranges' + (state.prevPreset ? ' d-none' : '')}>
-					<ul>
-						{props.presetRanges!.map((preset: IDateRange, idx: number) =>
-							<li key={idx} onClick={() => handlePresetClick(preset)} className={(preset.name === currentRange.name ? 'active' : '')}>
-								{preset.name}
+			<div className={'DateRangeDD ' + (props.className ?? '') + (props.borderless ? '' : ' border') + (props.showCaret ? ' dropdown-toggle' : '')}
+			     onClick={setOpen} ref={nodeParent} color={props.color}>
+				{props.faIcon !== null &&
+						<FontAwesomeIcon icon={props.faIcon ? props.faIcon : faCalendarAlt} fixedWidth />
+				} {rangeDescription(state.selectedRange!)}
+				<div className={ClassNames({DateRangeLB: true, OpensRight: !props.rightAlign, 'd-none': !state.isOpen})}
+				     ref={nodeBody}>
+					<div className={'ranges' + (state.prevPreset ? ' d-none' : '')}>
+						<ul>
+							{props.presetRanges!.map((preset: IDateRange, idx: number) =>
+									<li key={idx} onClick={() => handlePresetClick(preset)}
+									    className={(preset.name === currentRange.name ? 'active' : '')}>
+										{preset.name}
+									</li>
+							)}
+							<li onClick={handleCustomClick}>
+								{customRangeName}
+								<span className='float-end'>&gt;</span>
 							</li>
-						)}
-						<li onClick={handleCustomClick}>
-							{customRangeName}
-							<span className="float-end">&gt;</span>
-						</li>
-					</ul>
-				</div>
-				<div className={'drp-headers' + (!state.prevPreset ? ' d-none' : '')} onClick={handleUnCustomClick}>
+						</ul>
+					</div>
+					<div className={'drp-headers' + (!state.prevPreset ? ' d-none' : '')} onClick={handleUnCustomClick}>
                     <span>
                         &lt; Presets
                     </span>
-				</div>
-				<div className={'drp-calendar left' + (!state.prevPreset ? ' d-none' : '')}>
-					<div className="calendar-table">
-						
-						<DateRangeCalendar month={state.monthToShow} startSelected={state.customRange.start} endSelected={state.customRange.end} prevMonth={prevMonth} nextMonth={nextMonth} dateClick={handleDateClick} />
+					</div>
+					<div className={'drp-calendar left' + (!state.prevPreset ? ' d-none' : '')}>
+						<div className='calendar-table'>
+
+							<DateRangeCalendar month={state.monthToShow} startSelected={state.customRange.start}
+							                   endSelected={state.customRange.end} prevMonth={prevMonth}
+							                   nextMonth={nextMonth} dateClick={handleDateClick} />
+						</div>
+					</div>
+					<div className={'drp-buttons' + (!state.prevPreset ? ' d-none' : '')}>
+						<span className='drp-selected'>{rangeDescription(state.customRange)}</span>
+						<button className='btn btn-sm btn-primary' type='button'
+						        onClick={handleCustomApplyClick}>Apply
+						</button>
 					</div>
 				</div>
-				<div className={'drp-buttons' + (!state.prevPreset ? ' d-none' : '')}>
-					<span className="drp-selected">{rangeDescription(state.customRange)}</span>
-					<button className="btn btn-sm btn-primary" type="button" onClick={handleCustomApplyClick}>Apply</button>
-				</div>
 			</div>
-		</div>
 	)
 }
 
-export const defaultRanges: IDateRange[] = [
+export const DefaultRanges = (): IDateRange[] => [
 	{
 		name: 'This Week #' + moment().format('w'),
 		start: moment().startOf('week'),
@@ -364,9 +370,9 @@ export const defaultRanges: IDateRange[] = [
 	}
 ]
 
-export const defaultRangeStrings: IDateRangeString[] = defaultRanges.map(range => DateRangeToString(range))
+export const DefaultRangeStrings = (): IDateRangeString[] => DefaultRanges().map(range => DateRangeToString(range))
 
-export const defaultRangesReport: IDateRange[] = [
+export const DefaultRangesReport = (): IDateRange[] => [
 	{
 		name: 'This Week',
 		start: moment().startOf('week'),
@@ -399,9 +405,9 @@ export const defaultRangesReport: IDateRange[] = [
 	}
 ]
 
-export const defaultRangeStringsReport: IDateRangeString[] = defaultRangesReport.map(range => DateRangeToString(range))
+export const DefaultRangeStringsReport = (): IDateRangeString[] => DefaultRangesReport().map(range => DateRangeToString(range))
 
-export const defaultRangesReportQuarterly: IDateRange[] = [
+export const DefaultRangesReportQuarterly = (): IDateRange[] => [
 	{
 		name: 'This Month',
 		start: moment().startOf('month'),
@@ -454,64 +460,64 @@ export const defaultRangesReportQuarterly: IDateRange[] = [
 	}
 ]
 
-export const defaultRangeStringsReportQuarterly: IDateRangeString[] = defaultRangesReportQuarterly.map(range => DateRangeToString(range))
+export const DefaultRangeStringsReportQuarterly = (): IDateRangeString[] => DefaultRangesReportQuarterly().map(range => DateRangeToString(range))
 
 /**
  * Default to this month
  *
  * Use DateRangeToString(defaultRange) to get a string of it
  */
-export const defaultRange: IDateRange = {
+export const DefaultRange = (): IDateRange => ({
 	name: 'This Month',
 	start: moment().startOf('month'),
 	end: moment().endOf('month')
-}
+})
 
 /**
  * Default to last month
  *
  * Use DateRangeToString(defaultRange) to get a string of it
  */
-export const defaultRangeLastMonth: IDateRange = {
+export const DefaultRangeLastMonth = (): IDateRange => ({
 	name: 'Last Month',
 	start: moment().subtract(1, 'month').startOf('month'),
 	end: moment().subtract(1, 'month').endOf('month')
-}
+})
 
 /**
  * Default to this week
  *
  * Use DateRangeToString(defaultRangeWeek) to get a string of it
  */
-export const defaultRangeWeek: IDateRange = {
+export const DefaultRangeWeek = (): IDateRange => ({
 	name: 'This Week',
 	start: moment().startOf('week'),
 	end: moment().endOf('week')
-}
+})
 
 /**
  * Default to last 4 weeks
  *
  * Use DateRangeToString(defaultRangeLast4Weeks) to get a string of it
  */
-export const defaultRangeLast4Weeks: IDateRange = {
+export const DefaultRangeLast4Weeks = (): IDateRange => ({
 	name: 'Last 4 Weeks',
 	start: moment().subtract(3, 'week').startOf('week'),
 	end: moment().endOf('week')
-}
+})
 
 /**
  * Default to this year
  *
  * Use DateRangeToString(defaultRangeYear) to get a string of it
  */
-export const defaultRangeYear: IDateRange = {
+export const DefaultRangeYear = (): IDateRange => ({
 	name: 'Year-to-Date',
 	start: moment().startOf('year'),
 	end: moment().endOf('year')
-}
+})
 
-export const defaultRangeString = DateRangeToString(defaultRange)
+export const DefaultRangeString = () => DateRangeToString(DefaultRange())
 
 // DateRange.defaultProps = {
 // 	presetRanges: defaultRanges,
