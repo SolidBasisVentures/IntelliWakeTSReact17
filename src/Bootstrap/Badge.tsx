b7d2c9457a43b1bb534a9df0fdc8bc6a6e15aeb9
Dennis Peters <denjpeters@gmail.com>
2022-10-27 20:42:39 -0400

    Update Badge

--- src/Bootstrap/Badge.tsx
+++ src/Bootstrap/Badge.tsx
@@ -11,14 +11,9 @@ export interface IWBadgeProps extends Omit<React.HTMLProps<HTMLSpanElement>, 're
 export const Badge = (props: IWBadgeProps) => {
 	const TagToUse = props.tag ?? ('span' as React.ReactType)
 
-	let classes = props.className ?? ''
-	classes += !!props.color ? ` bg-${props.color}` : ''
-	classes +=
-		' ' +
-		ClassNames({
-			badge: true,
-			'rounded-pill': !props.notPill
-		})
-
-	return <TagToUse {...OmitProperty(props, 'tag', 'color', 'notPill', 'className')} className={classes.trim()} />
+	return <TagToUse {...OmitProperty(props, 'tag', 'color', 'notPill', 'className')}
+	                 className={`${props.className ?? ''} ${!!props.color ? ` bg-${props.color}` : ''} ${ClassNames({
+		                 badge: true,
+		                 'rounded-pill': !props.notPill
+	                 })}`.trim()} />
 }
