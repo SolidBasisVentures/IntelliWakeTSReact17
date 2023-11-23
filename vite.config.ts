606763006645af3172617d83970b030cce403b8c
Dennis Peters <dpeters@intelliwake.com>
2023-11-22 21:15:58 -0500

    Update vite and related plugins, add static file copying
    This commit updates the vite and related plugins, including vite, vite-plugin-dts, and vitest. A new plugin, vite-plugin-static-copy, has also been added. These changes were necessary to keep the project up-to-date with the latest dependencies and to enable the copying of static files during the build process. Also, the vite.config.ts file has been updated to use the new static file copying plugin. Dependencies and configurations in package.json and pnpm-lock.yaml were also updated accordingly.

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,12 @@
 import {resolve} from 'path'
 import {defineConfig} from 'vitest/config'
 import dts from 'vite-plugin-dts'
+import {viteStaticCopy} from 'vite-plugin-static-copy'
 
 export default defineConfig({
 	test: {},
 	build: {
-		target: "es2015",
+		target: 'es2015',
 		lib: {
 			entry: resolve(__dirname, 'src/main.ts'),
 			name: 'main',
@@ -13,6 +14,16 @@ export default defineConfig({
 		}
 	},
 	plugins: [
-		dts()
+		dts(),
+		viteStaticCopy({
+			targets: [
+				{src: 'src/IWake.scss', dest: 'dist'},
+				{
+					src: 'src/IWake-table-sticky.scss',
+					dest: 'dist'
+				},
+				{src: 'src/IWakeNoRoot.scss', dest: 'dist'}
+			]
+		})
 	]
 })
