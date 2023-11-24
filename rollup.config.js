38332bcea454491e1655fef05bd44823ab299a61
Dennis Peters <denjpeters@gmail.com>
2023-11-23 20:07:21 -0500

    Minor Fixes

--- rollup.config.js
+++ rollup.config.js
@@ -2,7 +2,7 @@ import typescript from 'rollup-plugin-typescript2'
 
 import scss from 'rollup-plugin-scss'
 import copy from 'rollup-plugin-copy'
-import pkg from './package.json'
+import pkg from './package.json' assert {type: 'json'}
 
 export default {
 	input: ['src/main.ts'],
